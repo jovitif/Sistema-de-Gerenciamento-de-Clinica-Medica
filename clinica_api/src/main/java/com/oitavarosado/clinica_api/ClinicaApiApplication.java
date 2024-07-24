9140ce7478d2d8406425e781c658f621d529360a
João Sales <joaosales911@gmail.com>
2024-07-24 18:15:15 -0300

    refactor: corrigindo UF para enum

--- clinica_api/src/main/java/com/oitavarosado/clinica_api/ClinicaApiApplication.java
+++ clinica_api/src/main/java/com/oitavarosado/clinica_api/ClinicaApiApplication.java
@@ -3,14 +3,10 @@ package com.oitavarosado.clinica_api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import io.github.cdimascio.dotenv.Dotenv;
-
-
 @SpringBootApplication
 public class ClinicaApiApplication {
 
 	public static void main(String[] args) {
-	    Dotenv dotenv = Dotenv.load();
 
 		SpringApplication.run(ClinicaApiApplication.class, args);
 	}
