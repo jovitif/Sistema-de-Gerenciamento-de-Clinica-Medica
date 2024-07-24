317f20b7b0272b16ea71f40f51b72795d62e1e6e
João Sales <joaosales911@gmail.com>
2024-07-24 16:49:21 -0300

    chore: configurando banco de dados local

--- clinica_api/src/main/java/com/oitavarosado/clinica_api/ClinicaApiApplication.java
+++ clinica_api/src/main/java/com/oitavarosado/clinica_api/ClinicaApiApplication.java
@@ -3,10 +3,15 @@ package com.oitavarosado.clinica_api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
+
 @SpringBootApplication
 public class ClinicaApiApplication {
 
 	public static void main(String[] args) {
+	    Dotenv dotenv = Dotenv.load();
+
 		SpringApplication.run(ClinicaApiApplication.class, args);
 	}
 
